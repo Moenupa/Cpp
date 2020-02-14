9a75890491bebeef60f3f171654d4db1e67c0b36
Moenupa <moenupa@outlook.com>
2020-02-14 17:41:35 +0800

    Cleanup
    Clean useless files and switch to VScode instead of VS2019

---
+++ salaryconvert.cpp
@@ -0,0 +1,17 @@
+﻿// a salary convertor in cpp
+// the main aim is to experience \b and \r
+
+#include <stdio.h>
+float getWeight();
+int main()
+{
+	getWeight();
+	float salary;
+	printf("\aEnter your desired monthly salary:");
+	printf(" $_______\b\b\b\b\b\b\b");
+	scanf_s("%f", &salary);
+	printf("\n\t$%.2f a month is $%.2f a year.", salary, salary * 12.0);
+	printf("\rGee!\n");
+	return 0;
+}
+
---
+++ talkback.cpp
@@ -0,0 +1,29 @@
+﻿// talkback.cpp -- interaction with users
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#define DENSITY 62.4
+
+int main()
+{
+	float weight, volume;
+	int size, letters;
+	char name[40];
+	// name be a char containing 40 strings
+
+	printf("Hi! What's your first name?\n");
+	scanf("%s", name);
+	printf("%s, what's your weight in pounds?\n", name);
+
+	scanf("%f", &weight);
+	size = sizeof name;
+	letters = strlen(name);
+	volume = weight / DENSITY;
+
+	printf("Well, %s, your volume is %2.2f cubic feet.\n", name, volume);
+	printf("Also, your first name has %d letters,\n", letters);
+	printf("And we have %d bytes to store it.\n", size);
+	system("pause");
+	return 0;
+
+}
