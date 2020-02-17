98a65f7f06909a13af3dd392fd730f45ef41ecc0
Moenupa <moenupa@outlook.com>
2020-02-17 16:35:55 +0800

    simple calculating program

---
+++ calculate.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int inte1, inte2;
+
+    cout << "input for the first integar: ____\b\b\b\b";
+    cin >> inte1;
+    cout << "input for the second integar: ____\b\b\b\b";
+    cin >> inte2;
+
+    cout << "Sum is " << inte1 + inte2 << endl;
+    cout << "Difference is " << inte1 - inte2 << endl;
+    cout << "Product is " << inte1 * inte2 << endl;
+    cout << "Quotient is " << inte1 / inte2 << endl;
+
+    return 0;
+}
