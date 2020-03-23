839392489e75f61e2303b5a856799c8f1bf919cf
Moenupa <mineworldy@outlook.com>
2020-03-23 09:46:47 +0800

    Simplify: file movement

--- temp.cpp
+++ temp.cpp
@@ -1,25 +1,14 @@
 #include <iostream>
-#include <stdio.h>
 
 using namespace std;
-void printReverse(const char *str) {
-    if (!*str) {
-        return;
-    }
-    printReverse(str+1);
-    putchar(*str);
-}
 
-int main() {
-    char str[100];
-    cout << "Input string: ";
-    while (cin >> str) {
-        char ch = getchar();
-        if (ch != '\n') {
-            break;
-        }
-    }
-    cout << str;
-    printReverse(str);
-}
+int main () {
+    const int SIZE = 5;
 
+    int squares[SIZE] = {0, 1, 4, 9, 16};
+    int lucky_numbers[SIZE];
+
+    for (int i = 0; i < SIZE; i++) {
+        lucky_numbers[i] = squares[i];
+    }
+}
