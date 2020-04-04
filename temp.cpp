666a8b21239a96e98c30f905965f381d85d0178c
Moenupa <mineworldy@outlook.com>
2020-04-04 18:00:38 +0800

    some tryouts

--- temp.cpp
+++ temp.cpp
@@ -1,14 +1,23 @@
-#include <iostream>
 
-using namespace std;
-
-int main () {
-    const int SIZE = 5;
-
-    int squares[SIZE] = {0, 1, 4, 9, 16};
-    int lucky_numbers[SIZE];
-
-    for (int i = 0; i < SIZE; i++) {
-        lucky_numbers[i] = squares[i];
+/*
+for (int count = 1; count <= maxOddNums; count++) {
+    if ((earlyStop = rand() / 100) < (curOddNum = count * 2 - 1)) {
+        break;
     }
+    else if (count == 1) {
+        cout << curOddNum;
+    }
+    else {
+        cout << ", " << curOddNum;
+    }
+}*/
+
+int count = 1;
+while ( (count<=maxOddNums) && ((rand() / 100)>=(count * 2 - 1)) ) {
+    if ( count == 1 ) {
+        cout << count * 2 - 1;
+    } else {
+        cout << ", " << count * 2 - 1;
+    }
+    cout++;
 }
