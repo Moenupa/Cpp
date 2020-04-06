80fc51f80bc2a6bfb38bf2784172d9df6430a70d
Moenupa <mineworldy@outlook.com>
2020-04-06 16:56:27 +0800

    mingw in program files set up

--- temp.cpp
+++ temp.cpp
@@ -1,23 +1,7 @@
+#include <iostream>
 
-/*
-for (int count = 1; count <= maxOddNums; count++) {
-    if ((earlyStop = rand() / 100) < (curOddNum = count * 2 - 1)) {
-        break;
-    }
-    else if (count == 1) {
-        cout << curOddNum;
-    }
-    else {
-        cout << ", " << curOddNum;
-    }
-}*/
+using namespace std;
 
-int count = 1;
-while ( (count<=maxOddNums) && ((rand() / 100)>=(count * 2 - 1)) ) {
-    if ( count == 1 ) {
-        cout << count * 2 - 1;
-    } else {
-        cout << ", " << count * 2 - 1;
-    }
-    cout++;
+int main () {
+    cout << "Hello World";
 }
