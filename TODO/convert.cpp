b90ae8267417d13be01150f551642b11fad82fa3
Moenupa <mineworldy@outlook.com>
2020-04-12 17:30:30 +0800

    comments and requirements for convert.cpp

---
+++ TODO/convert.cpp
@@ -0,0 +1,15 @@
+/*
+Converting a sequence of values
+
+What unit do you want to convert from? #cm
+What unit do you want to convert to? #in
+Enter values, terminated by 0: #30
+## 30 cm = 11.81 in
+Enter values, terminated by 0: #100
+## 100 cm = 39.37 in
+Enter values, terminated by 0: #0
+What unit do you want to convert from?
+
+From unit (in, ft, mi, mm, cm, m, km, oz, lb, g, kg, tsp, tbsp, pint, gal): #cm
+To unit: #in
+*/
