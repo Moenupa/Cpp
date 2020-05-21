7f46b73556a2112595527a00a1d4ee68d86c2931
Moenupa <mineworldy@outlook.com>
2020-05-21 13:50:50 +0800

    📘1011 A1 附加文档可用
    Fxxk1011😒

---
+++ fxxk1011/19078543d_WANGMeng/Readme.txt
@@ -0,0 +1,25 @@
+GENERAL INFORMATION
+project: 723 Cafe Order System
+name: WANG Meng
+ID: 19078543D
+Date: May 21, 2020
+
+FILES INCLUDED
+
+assign1_main.cpp
+                        Source Code in C++
+                        Environment: C++14, gcc 8.1.0
+
+assign1_main_out.pdf
+                        Output Sample in Graphs
+
+notes_to_owner.pdf
+                        Notes to Owner
+
+user_guide.pdf
+                        Step-to-Step Procedure Details
+
+assign1_main_sample.exe
+                        Sample Build Exe of the source code
+                        Run/Test in Bash/Powershell/Cmd with the ./ command
+                        DO NOT run in internal consoles(like vscode internal console)
--- fxxk1011/19078543d_WANGMeng/assign1_main.cpp
+++ fxxk1011/19078543d_WANGMeng/assign1_main.cpp
@@ -5,6 +5,7 @@ do NOT run in INTEGRATED terminals (system("cls") will cause problem)
 @author Wang Meng
 @date 2020-04-30
 @version 2.1
+@debug through gcc8.1.0 with c++14(default)
 */
 #include <iostream>
 #include <iomanip>
@@ -88,17 +89,17 @@ map<string, int> price = {{"A1", 30}, {"A2", 34}, {"A3", 32}, {"B1", 28}, {"B2",
 
 class NewOrder {
     public:
-        string ois;                                     // customer ois
-        map<string, int> order;                         // order details
-        int total;                                      // order price
+        string ois;                                                                             // customer ois
+        map<string, int> order;                                                                 // order details
+        float total;                                                                            // order price
     public:
-        void init(string oisin)                         // initiate
+        void init(string oisin)                                                                 // initiate
         {
             order = {{"A1", 0}, {"A2", 0}, {"A3", 0}, {"B1", 0}, {"B2", 0}, {"B3", 0}, {"C1", 0}, {"C2", 0}, {"C3", 0}, {"C4", 0}, {"C5", 0}, {"S1", 0}, {"S2", 0}, {"S3", 0}, {"S4", 0}, {"S5", 0}};
             total = 0;
             ois = oisin;
         }
-        void mergeToSet()                               // merge seperate order to a set
+        void mergeToSet()                                                                       // merge seperate order to a set
         {
             while (order["C1"]!=0 && order["B2"]!=0) {
                 --order["C1"];
@@ -133,8 +134,10 @@ class NewOrder {
                 ++order["S5"];
             }
         }
-        int cal_total()                                 // calculate total order price
+        float cal_total()                                                                       // calculate total order price
         {
+
+            total = 0;
             for (auto iter = order.begin(); iter != order.end(); iter++)
             { // iterate through all the ITEM
                 total += price[iter->first] * iter->second;
@@ -142,24 +145,24 @@ class NewOrder {
             return total;
         }
 };
-vector<NewOrder> orders = {};                           // all orders will be saved to this vector
+vector<NewOrder> orders = {};                                                                   // all orders will be saved to this vector
 // NOTE: in real application this should be put into a file for transmission
-string buf;                                             // handle input and prevent excecptions
+string buf;                                                                                     // handle input and prevent excecptions
 // init global class type and variables
 
-void printCmd(string token);                            // print Command details
-void printOrd(string token, int repeatTimes);           // print order details
-void clearTerminal();                                   // clear the terminal
+void printCmd(string token);                                                                    // print Command details
+void printOrd(string token, int repeatTimes);                                                   // print order details
+void clearTerminal();                                                                           // clear the terminal
 // declear used functions
 
 int main () {
     #ifdef _WIN32
-        SetConsoleOutputCP (65001);                      // ensure utf-8 character display normally
+        SetConsoleOutputCP (65001);                                                             // ensure utf-8 character display normally
         CONSOLE_FONT_INFOEX info = { 0 };
         info.cbSize = sizeof(info);
-        info.dwFontSize.Y = 16;                         // leave X as zero
+        info.dwFontSize.Y = 16;                                                                 // leave X as zero
         info.FontWeight = FW_NORMAL;
-        wcscpy(info.FaceName, L"Consolas");             // set a font that is adequate for display
+        wcscpy(info.FaceName, L"Consolas");                                                     // set a font that is adequate for display
         SetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE), NULL, &info);
     #endif
     // ensure utf-8 character support
@@ -168,31 +171,37 @@ int main () {
     string cmd, tempOis, tempOrd = "";
     // local variabel to indicate current customer handle input
 
-    SYSIN:                                              // exit: exit or order
+    SYSIN:                                                                                      // exit: exit or order
         clearTerminal();
         printCmd("-E");
         cout << "┃ Enter command index ┃ _\b";
         cin >> cmd;
-        if (cmd.compare("0")==0) {                      // 0 exit
+        if (cmd.compare("0")==0)                                                                // 0 exit
+        {
             cout << "Exit System." << endl;
             return 0;
-        } else if (cmd.compare("1")==0) {               // 1 start order
+        }
+        else if (cmd.compare("1")==0)                                                           // 1 start order
+        {
             clearTerminal();
             goto OISIN;
-        } else {                                        // else loop back
+        }
+        else                                                                                    // else loop back
+        {
             cout << "┃ INVALID INPUT ┃ Please try again." << endl;
             this_thread::sleep_for(chrono::seconds(2));
             goto SYSIN;
         }
-    OISIN:                                              // ois: input ois and check legal
+    OISIN:                                                                                      // ois: input ois and check legal
         cout << "┃ Personalize your Pick-Up Code (e.g.<oh my god>) ┃\n┠ ________\b\b\b\b\b\b\b\b";
         cin.clear();
         cin.sync();
         getline(cin, tempOis);
         cur_cus = orders.size();
-        for (int i = 0; i < cur_cus; i++)               // check if ois occupied
+        for (int i = 0; i < cur_cus; i++)                                                       // check if ois occupied
         {
-            if (tempOis.compare(orders[i].ois)==0) {
+            if (tempOis.compare(orders[i].ois)==0)
+            {
                 cout << "Pick-Up Code already exists, try another one." << endl;
                 this_thread::sleep_for(chrono::seconds(2));
                 clearTerminal();
@@ -200,84 +209,106 @@ int main () {
             }
         }
         orders.push_back(NewOrder());
-        orders[cur_cus].init(tempOis);                  // create new NewOrder and set its ois
+        orders[cur_cus].init(tempOis);                                                          // create new NewOrder and set its ois
         clearTerminal();
-    ORDIN:                                              // order: ask for item code
+    ORDIN:                                                                                      // order: ask for item code
         printCmd("-I");
         cout << "┃ Enter Item Code ┃ __\b\b";
         cin >> tempOrd;
-        if (orders[cur_cus].order.find(tempOrd) != orders[cur_cus].order.end()) {
-            orders[cur_cus].order[tempOrd] += 1;        // if valid add the food by 1
-        } else {                                        // else loop back order
+        if (tempOrd.compare("0") == 0)
+        {
+            cout << "┃ NULL INPUT ┃" << endl;
+            this_thread::sleep_for(chrono::seconds(2));
+        }
+        else if (orders[cur_cus].order.find(tempOrd) != orders[cur_cus].order.end())                 // if valid add the food by 1
+        {
+            orders[cur_cus].order[tempOrd] += 1;
+        }
+        else                                                                                    // else loop back order
+        {
             cout << "┃ INVALID INPUT ┃ Please try again." << endl;
             this_thread::sleep_for(chrono::seconds(2));
             goto ORDIN;
         }
-    ORDLOOP:                                            // orderloop: continue or stop ordering
+    ORDLOOP:                                                                                    // orderloop: continue or stop ordering
         printCmd("-O");
-        cout << "┃ Enter Order Command Index ┃ _\b";
+        cout << "┃ Enter Order Command Index ┃  _\b";
         cin >> cmd;
-        if (cmd.compare("0") == 0)                      // stop: save the order, then return to sysinput
+        if (cmd.compare("0") == 0)                                                              // stop: save the order, then return to sysinput
         {
             clearTerminal();
             orders[cur_cus].mergeToSet();
             printCmd("-R");
-            for (auto iter = orders[cur_cus].order.begin(); iter != orders[cur_cus].order.end(); iter++) {                                  // go through all ITEM
+            for (auto iter = orders[cur_cus].order.begin(); iter != orders[cur_cus].order.end(); iter++)
+            {                                                                                   // go through all ITEM
                 printOrd(iter->first,iter->second);
             }
             cout << "╟──────────┴────────────────────────────────┴───────╢" << endl;
-            cout << "║ PickUpCode:   " << setw(35) << orders[cur_cus].ois << " ║"<< endl;
-            cout << "║ Total:  $" << setw(40) << fixed << setprecision(2) << orders[cur_cus].cal_total()/1.0 << " ║"<< endl;
+            cout << "║ PickUpCode: " << setw(37) << orders[cur_cus].ois << " ║"<< endl;
+            orders[cur_cus].cal_total();
+            if (orders[cur_cus].total > 100)
+            {
+                cout << "║ Total before Discount: " << setw(16) << "$" << setw(10) << fixed << setprecision(2) << orders[cur_cus].total / 1.0 << " ║" << endl;
+                cout << "║ Discount of 5%: " << setw(23) << "-$" << setw(10) << fixed << setprecision(2) << orders[cur_cus].total / 1.0 * 0.05 << " ║" << endl;
+                orders[cur_cus].total *= 0.95;
+            }
+            cout << "║ Total: " << setw(32) << "$" << setw(10)<< fixed << setprecision(2) << orders[cur_cus].total/1.0 << " ║" << endl;
             printCmd("-S");
             goto PAYIN;
-        } else if (cmd.compare("1") == 0) {             // make order, then setback to ordinput
+        }
+        else if (cmd.compare("1") == 0)                                                         // make order, then setback to ordinput
+        {
             clearTerminal();
             goto ORDIN;
-        } else {                                        // invalid input - loop back to ordinput
+        }
+        else                                                                                    // invalid input - loop back to ordinput
+        {
             clearTerminal();
             cout << "┃ INVALID INPUT ┃ Please try again." << endl;
             this_thread::sleep_for(chrono::seconds(2));
             goto ORDLOOP;
         }
-    PAYIN:                                              // pay: confirm order and pay (payment skipped)
+    PAYIN:                                                                                      // pay: confirm order and pay (payment skipped)
         printCmd("-P");
-        cout << "┃ Enter Payment Command Index ┃ _\b";
+        cout << "┃ Enter Payment Command Index ┃  _\b";
         cin >> cmd;
-        if (cmd.compare("0") == 0)                      // if cancel 0, then return to sysin
+        if (cmd.compare("0") == 0)                                                              // if cancel 0, then return to sysin
         {
             orders.pop_back();
             cout << "┃ Payment Canceled ┃" << endl;
             this_thread::sleep_for(chrono::seconds(2));
             goto SYSIN;
-        } else if (cmd.compare("1") == 0) {             // if confirmed 1, then return to sysin
-            cout << "┃ Payment Confirmed ┃\n┃ Thanks for coming to 7-23 Cafe. See you next time. ┃" << endl;
-            this_thread::sleep_for(chrono::seconds(4)); // sleep for 4 seconds
+        }
+        else if (cmd.compare("1") == 0)                                                         // if confirmed 1, then return to sysin
+        {
+            cout << "┃ Payment Confirmed ┃" << endl;
+            this_thread::sleep_for(chrono::seconds(4));                                         // sleep for 4 seconds
             goto SYSIN;
-        } else {
+        }
+        else
+        {
             cout << "┃ INVALID INPUT ┃ Please try again." << endl;
             this_thread::sleep_for(chrono::seconds(2));
             goto PAYIN;
         }
 }
-void printCmd(string token)                             // print Command details
+void printCmd(string token)                                                                     // print Command details
 {
     bool flag = false;
-    for (int i = 0; i < INFO.size(); i++)               // loop through INFO
+    for (int i = 0; i < INFO.size(); i++)                                                       // loop through INFO
     {
         buf = INFO[i];
-        if (buf.compare(0,2,token)==0)                  // if token detected, start or end printing
-        {flag = !flag;}
-        else if (flag) { cout << buf << endl; }
+        if (buf.compare(0,2,token)==0) {flag = !flag;}                                          // if token detected, start or end printing
+        else if (flag) {cout << buf << endl;}
     }
 }
-void printOrd(string token, int repeatTimes)            // print order details
+void printOrd(string token, int repeatTimes)                                                    // print order details
 {
     if (repeatTimes!=0)
     {
         for (int i = 0; i < INFO.size(); i++)
         {
             buf = INFO[i];
-
             if (buf.find(token, 4) != string::npos)
             {
                 for (int j = 0; j < repeatTimes; j++) {cout << buf << endl;}
@@ -285,7 +316,7 @@ void printOrd(string token, int repeatTimes)            // print order details
         }
     }
 }
-void clearTerminal ()                                   // clear the terminal
+void clearTerminal ()                                                                           // clear the terminal
 {
     #ifdef _WIN32
         system("cls");
